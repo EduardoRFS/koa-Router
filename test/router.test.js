4d7ab54ec5d64f9c8533e7c1ee9010de889373fe
EduardoRFS <theeduardorfs@gmail.com>
2016-10-23 03:05:20 -0200

    Revert "inca refactor"
    This reverts commit 4aed08cde1276282cc4d23d6d8bb15772ffb56a1.

--- test/router.test.js
+++ test/router.test.js
@@ -117,7 +117,7 @@ describe('Router', function() {
     const router = new Router();
     const router2 = new Router();
 
-    router2.post('/test', ctx => {
+    router2.post('/', ctx => {
       throw new Error('Multi Magic Error');
     });
 
@@ -126,19 +126,19 @@ describe('Router', function() {
         .catch(err => ctx.body = err.message);
     });
     router.use(router2);
-    router.get('/test', ctx => {
+    router.get('/', ctx => {
       throw new Error('Magic Error');
     });
 
     app.use(router);
     it('catch', done => {
       request(app.listen())
-        .get('/test')
+        .get('/')
         .expect('Magic Error', done);
     });
     it('multi layer catch', done => {
       request(app.listen())
-        .post('/test')
+        .post('/')
         .expect('Multi Magic Error', done);
     });
   });
